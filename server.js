84ed8465d7d3d85a789d386e4c40d582ce61d464
Sandrome <sudayavarman@salesforce.com>
2019-10-18 00:45:46 +0530

    Version 1.4

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/', function (req, res) {
     });
   })
   
-  app.listen(5000, function () {
+  app.listen(process.env.PORT, function () {
     console.log('Example app listening on port 3000!')
   })
